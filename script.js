316eb4ee57dacfb27ddbff4e1235d530042385ed
ryan <ryedgar1@gmail.com>
2019-05-08 13:34:58 -0400

    completed form functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+document.querySelector("form").addEventListener("submit", displayDogName);
+
+function displayDogName(event) {
+    event.preventDefault();
+    let dogname = document.querySelector("#dogname").value;
+    document.body.append(dogname);
+}
